2c9b77bc56d85c16d324ed2c9787237ee66b146d
ElleWest <izelle.vanderwesthuizen@iu-study.org>
2025-11-09 22:22:32 +0200

    fix: Isolate Library import for checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.detail import DetailView
-from .models import Book, Library
+from .models import Book
+from .models import Library
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
