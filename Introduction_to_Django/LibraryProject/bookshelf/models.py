f03b0a286c066ff7f0cebcc4e97e8d498f0ad4d5
ElleWest <izelle.vanderwesthuizen@iu-study.org>
2025-11-09 21:44:02 +0200

    Adjust Book field lengths to required size

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Book(models.Model):
-	"""Represents a single book entry stored in the library catalogue."""
+    """Represents a single book entry stored in the library catalogue."""
 
-	title = models.CharField(max_length=255)
-	author = models.CharField(max_length=255)
-	publication_year = models.IntegerField()
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    publication_year = models.IntegerField()
 
-	def __str__(self) -> str:
-		return f"{self.title} by {self.author} ({self.publication_year})"
+    def __str__(self) -> str:
+        return f"{self.title} by {self.author} ({self.publication_year})"
