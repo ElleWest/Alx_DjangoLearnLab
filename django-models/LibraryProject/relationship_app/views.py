04906cba02024241c6a1cf87753bd0453d06d24c
ElleWest <izelle.vanderwesthuizen@iu-study.org>
2025-11-09 22:00:41 +0200

    feat: Implement Django views and URL configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book, Library
 
-# Create your views here.
+def list_books(request):
+    """
+    A function-based view that lists all books.
+    """
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+class LibraryDetailView(DetailView):
+    """
+    A class-based view that displays details for a specific library.
+    """
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
