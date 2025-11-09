150b6178353f80ba50b2ec4f662fe00e24a60558
ElleWest <izelle.vanderwesthuizen@iu-study.org>
2025-11-09 22:24:07 +0200

    fix: Use explicit view imports in urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,17 +1,27 @@
 from django.urls import path
-from . import views
+from .views import (
+    list_books,
+    LibraryDetailView,
+    register,
+    admin_view,
+    librarian_view,
+    member_view,
+    add_book,
+    edit_book,
+    delete_book,
+)
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', views.register, name='register'),
-    path('admin_view/', views.admin_view, name='admin_view'),
-    path('librarian_view/', views.librarian_view, name='librarian_view'),
-    path('member_view/', views.member_view, name='member_view'),
-    path('add_book/', views.add_book, name='add_book'),
-    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
-    path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
+    path('register/', register, name='register'),
+    path('admin_view/', admin_view, name='admin_view'),
+    path('librarian_view/', librarian_view, name='librarian_view'),
+    path('member_view/', member_view, name='member_view'),
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:pk>/', edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', delete_book, name='delete_book'),
 ]
